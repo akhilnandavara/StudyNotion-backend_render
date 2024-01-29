a3db221c2cd9f357c54de6814366cb33fbc29c18
akhilnandavara <akhiln1108@gmail.com>
2024-01-30 00:02:39 +0530

    depolyment url updated

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ app.use(express.json());// json parser
 app.use(cookieParser());// cookie parser
 
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://studynotion-edutechlearning.onrender.com",
     credentials:true,
 }))
-// origin:"https://studynotion-edutechlearning.onrender.com",
+// origin:"http://localhost:3000",
 
 app.use(
     fileUpload({
